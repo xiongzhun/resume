ab7b60c2d7ebdc20881bcabe6df944375040b394
WuHaolin <gwuhaolin@gmail.com>
2015-08-06 23:19:19 +0800

    修正信息

--- i18n/zh-CN/dict.js
+++ i18n/zh-CN/dict.js
@@ -37,7 +37,7 @@ module.exports = {
     lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
     dict: {
         source_code: "源代码",
-        demo: "Demo"
+        demo: "演示"
     },
     project_type: {
         "personal": "个人项目"
